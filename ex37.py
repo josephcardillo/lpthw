e78c0d48108b74fe08b4307627d164de2479f9b1
Joseph Cardillo <joseph.s.cardillo@gmail.com>
2020-12-10 15:00:39 -0500

    ex37 update

--- ex37.py
+++ ex37.py
@@ -1,6 +1,7 @@
 # and
 # assert - https://docs.python.org/3.7/reference/simple_stmts.html#the-assert-statement
     # asserts something is true, and if not, throws assertion error
+    # https://www.programiz.com/python-programming/assert-statement
 
 coffee = True
 sleep = False
@@ -29,29 +30,116 @@ def drink_coffee():
         print("I don't understand what that means.")
 
 # break statement
-death = False
+
 def live_or_die():
+    death = False
     while death == False:
         print("You drank to much coffee.")
         print("Will you drink some more?")
         more_coffee = input("> ")
         if "yes" in more_coffee:
-            print("Now you've done it. Goodbye!")
+            print("Now you've done it.")
+            print(f"You should eat an {Fruits.a} instead.")
+            print(f"And maybe a {Fruits.b}, too.")
+            print("Goodbye!")
             break
         elif "no" in more_coffee:
             print("Good choice.")
+            death = True
         else:
             print("I don't understand that.")
 
-health_check()
+# class - https://docs.python.org/3.7/tutorial/classes.html
+    # This is a very simply way to incorporate classes, but for the purpose of learning:
+
+class Fruits:
+    a = "Apple"
+    b = "Banana"
+    c = ["orange", "pineapple", "watermelon"]
+    d = ["dates", "apple", "banana", "pineapple", "mange", "watermelon"]
+
+# del - https://docs.python.org/3.7/tutorial/datastructures.html#the-del-statement
+
+# del d[0] - deletes element at 0 index
+# del d[0:4] - deletes elements at from index 0 to 3
+# del d[:] - deletes everything in list
+
+def list_fruits(f):
+    for fruit in f:
+        print(fruit)
+
+# after importing the function into python
+# from ex37 import *
+
+# >>> list_fruits(Fruits.c)
+# orange
+# pineapple
+# watermelon
+
+# Otherwise, if I do:
+# import ex37
+# I need to do this to list them:
+# >>> ex37.list_fruits(ex37.Fruits.c)
+# orange
+# pineapple
+# watermelon
+
+# continue - https://docs.python.org/3.7/tutorial/controlflow.html#break-and-continue-statements-and-else-clauses-on-loops
+
+
+def weather():
+    outside = True
+    while outside == True:
+        outside = input("Are you outside or inside? > ")
+        if outside == "outside":
+            outside = True
+            continue
+        elif outside == "inside":
+            print("Just in time because a terrible storm is coming.")
+            outside = False
+        else:
+            print("Try again.")
+            outside = True
+            continue
+
+def print_even_num():
+    for num in range(2, 10):
+        if num % 2 == 0:
+            print("You found an even number:", num)
+            continue
+        print(f"{num} is not even.")
+
+def find_even_num():
+    num = int(input("Enter a number: > "))
+    if num % 2 != 0:
+        print("Not an even number.")
+        find_even_num()
+    else:
+        print(f"Yay! You found an even number: {num}")
 
 # as - part of the with statement
     # https://docs.python.org/3.7/whatsnew/2.6.html#pep-343-the-with-statement
 
-with open("test.txt", 'r') as file:
-    for line in file:
-        stripped = line.strip()
-        new_line = stripped + ' wtf'
-        print(new_line)
+def with_statement():
+    with open("test.txt", 'r') as file:
+        for line in file:
+            stripped = line.strip()
+            new_line = stripped + ' yo.'
+            print(new_line)
 
+# except statement - https://docs.python.org/3.7/tutorial/errors.html
 
+def integer_test():
+    while True:
+        try:
+            num = int(input("Please enter a number: "))
+            break
+        except ValueError:
+            print("Oops!  That was no valid number.  Try again...")
+
+# find_even_num()
+# find_even_num()
+# health_check()
+# weather()
+# with_statement()
+integer_test()
